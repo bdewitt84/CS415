e59785473c09e1e3a631d5edc6b31f2e2e57007b
CS student <me@CSstudent.local>
2024-10-03 21:04:24 -0700

    Attempted to implement str_filler() and free_command_line(), lab1.exe still segfaults

--- Lab01/string_parser.c
+++ Lab01/string_parser.c
@@ -30,7 +30,11 @@ int count_token (char* buf, const char* delim)
 	int count = 0;
 
 	//check for null string
+	if (buf == NULL) {
+		return 0;
+	}
 
+	// iterate through string counting tokens
 	for (str = buf; ;str = NULL) {
 		token = strtok_r(str, delim, &saveptr);
 		if (token == NULL) {
@@ -38,30 +42,45 @@ int count_token (char* buf, const char* delim)
 		}
 		count++;
 	}
+
+	// return the number of token
 	return count;
 }
 
-command_line str_filler (char* buf, const char* delim)
-{
-	//TODO：
-	/*
-	*	#1.	create command_line variable to be filled and returned
-	*	#2.	count the number of tokens with count_token function, set num_token. 
-    *           one can use strtok_r to remove the \n at the end of the line.
-	*	#3. malloc memory for token array inside command_line variable
-	*			based on the number of tokens.
-	*	#4.	use function strtok_r to find out the tokens 
-    *   #5. malloc each index of the array with the length of tokens,
-	*			fill command_list array with tokens, and fill last spot with NULL.
-	*	#6. return the variable.
-	*/
-}
+command_line str_filler (char* buf, const char* delim) {	
 
+    command_line result;
+
+    // make copy because the buffer will be consumed by the function
+    char* buf_cpy = strdup(buf);
+    int num_token = count_token(buf_cpy, delim);
+
+    result.num_token = num_token;
+    result.command_list = malloc((num_token + 1) * sizeof(char*));
+
+    char *str, *token, *saveptr;
+    int i = 0;
+    
+    for (str = buf; ; str = NULL) {
+        token = strtok_r(str, delim, &saveptr);
+        if (token == NULL) {
+            break;
+        }
+        result.command_list[i] = malloc((strlen(token) + 1) * sizeof(char));
+        strcpy(result.command_list[i], token);
+        i++;
+    }
+
+    result.command_list[num_token] = NULL;
+
+    return result;
+}
 
 void free_command_line(command_line* command)
 {
-	//TODO：
-	/*
-	*	#1.	free the array base num_token
-	*/
+	for (int i = 0; i < command->num_token; i++) {
+		free(command->command_list[i]);
+	}
+	free(command->command_list);
 }
+
