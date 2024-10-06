424614151ec83bbd295f87e37a677e6e8e10e842
CS student <me@CSstudent.local>
2024-10-06 16:00:28 -0700

    added stub for main.c

---
+++ lab02/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ * Created on 10/6/20204
+ * Author: Brett DeWitt
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "string_parser.h"
+#include "command.h"
+
+int main(int argc, char const *argv[]) {
+	
+
+
+	return 0;
+}
